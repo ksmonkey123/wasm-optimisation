c9e0addb8c9ce2beaa48031dcff678d47f44a8c1
Andreas Wälchli <andreas.waelchli@me.com>
2019-02-28 10:50:50 +0100

    created higher-level module type

--- src/ch/awae/wasm/ast/Module.scala
+++ src/ch/awae/wasm/ast/Module.scala
@@ -2,21 +2,67 @@ package ch.awae.wasm.ast
 
 import scala.annotation.tailrec
 
-case class Module(sections: List[Section])
+case class RawModule(sections: List[Section])
 
-case object Module {
+case object RawModule {
 
   val signature: List[Byte] = List(0x00, 0x61, 0x73, 0x6d, 0x01, 0x00, 0x00, 0x00)
 
-  private[ast] def apply(stream: DataStream): Module =
+  private[ast] def apply(stream: DataStream): RawModule =
     if (stream.take(8) != signature)
       throw new IllegalArgumentException
     else
-      Module(readSections(stream))
+      RawModule(readSections(stream))
 
   @tailrec
   private def readSections(stream: DataStream, acc: List[Section] = Nil): List[Section] = stream.takeOptional match {
     case Some(x) => readSections(stream, Section(x, stream) :: acc)
     case None    => acc.reverse
   }
+}
+
+case class Module(rawSections: List[Section], types: TypeSection, functions: List[WasmFunction])
+
+object Module {
+  private[ast] def apply(stream: DataStream): Module = {
+    @tailrec
+    def scan(
+      sections:  List[Section],
+      remainder: List[Section]   = Nil,
+      types:     TypeSection     = null,
+      funcs:     FunctionSection = null,
+      codes:     CodeSection     = null,
+      impos:     ImportSection   = null): Module = {
+      if (sections.isEmpty)
+        collect(remainder.reverse, types, funcs, codes, impos)
+      else
+        sections.head match {
+          case x: TypeSection     => scan(sections.tail, remainder, x, funcs, codes, impos)
+          case x: FunctionSection => scan(sections.tail, remainder, types, x, codes, impos)
+          case x: CodeSection     => scan(sections.tail, remainder, types, funcs, x, impos)
+          case x: ImportSection   => scan(sections.tail, remainder, types, funcs, codes, x)
+          case x                  => scan(sections.tail, x :: remainder, types, funcs, codes, impos)
+        }
+    }
+
+    def collect(remainder: List[Section], types: TypeSection, funcs: FunctionSection, codes: CodeSection, impos: ImportSection): Module = {
+      val declaredFunctions = funcs.typeIndices zip codes.functions map {
+        case (tIdx, Code(locals, body)) => DeclaredFunction(tIdx, locals, body)
+      }
+      val otherImports = Option(impos) map { _.imports filter { !_.desc.isInstanceOf[TypeDesc] } }
+      val functionImports = Option(impos) map {
+        _.imports filter { _.desc.isInstanceOf[TypeDesc] } map {
+          case ImportEntry(mod, name, TypeDesc(typeId)) => ImportedFunction(typeId, mod, name)
+          case _                                        => ???
+        }
+      }
+      
+      Module(if (otherImports.isDefined) ImportSection(otherImports.get) :: remainder else remainder, types,
+        if (functionImports.isDefined) functionImports.get ::: declaredFunctions else declaredFunctions)
+    }
+
+    scan(RawModule(stream).sections)
+
+  }
+
 }
---
+++ src/ch/awae/wasm/ast/WasmFunction.scala
@@ -0,0 +1,6 @@
+package ch.awae.wasm.ast
+
+trait WasmFunction { def typeIdx: Int }
+
+case class ImportedFunction(typeIdx: Int, mod: String, name: String) extends WasmFunction
+case class DeclaredFunction(typeIdx: Int, locals: List[Locals], body: Expression) extends WasmFunction
