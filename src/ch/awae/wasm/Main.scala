06a8b7213510ba84b17b770dafae7e4e9a9e4207
Andreas Wälchli <andreas.waelchli@me.com>
2019-04-04 22:33:24 +0200

    jgrapht library

--- src/ch/awae/wasm/Main.scala
+++ src/ch/awae/wasm/Main.scala
@@ -4,8 +4,38 @@ import ch.awae.wasm.ast.WasmFunction.DeclaredFunction
 import ch.awae.wasm.io.implicits._
 import ch.awae.wasm.ast.implicits._
 import ch.awae.wasm.util.Dot
+import org.jgrapht.graph.{DefaultDirectedGraph, DefaultEdge}
+import org.jgrapht.traverse.TopologicalOrderIterator
+
+import scala.collection.JavaConverters.asScalaIteratorConverter
+import scala.collection.mutable.ListBuffer
 
 object Main extends App {
+
+  val graph = new DefaultDirectedGraph[String, DefaultEdge](classOf[DefaultEdge]);
+
+  graph.addVertex("a")
+  graph.addVertex("b")
+  graph.addVertex("c")
+  graph.addVertex("d")
+  graph.addVertex("e")
+  graph.addVertex("f")
+
+  graph.addEdge("a", "b")
+  graph.addEdge("a", "c")
+  graph.addEdge("b", "c")
+  graph.addEdge("e", "b")
+  graph.addEdge("c", "f")
+  graph.addEdge("e", "f")
+  graph.addEdge("b", "d")
+
+  val iterator = new TopologicalOrderIterator(graph)
+  val nodes = ListBuffer.empty
+  val ordering = iterator.asScala.toList
+
+  println(ordering)
+
+  System exit 0
 
   val module = "change.wasm".file.ast.module
   val functions = module.funcs.filter(_.isInstanceOf[DeclaredFunction]).map(_.asInstanceOf[DeclaredFunction])
